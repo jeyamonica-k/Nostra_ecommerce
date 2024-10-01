ac717a9918ce7fb7bc3c9ea88e69f928e5cdaab1
monicakodeeswaran32@gmail.com <monicakodeeswaran32@gmail.com>
2024-10-01 23:48:17 +0530

    added js

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ var slideimage=document.querySelector(".hero_container_img")
 var offer_section=document.querySelector(".offer_section")
 var offer_section_close=document.getElementById("offer_section_close")
 
+var Navbar_container_sub3_menu=document.getElementById("Navbar_container_sub3_menu")
+var Sidenavbar=document.querySelector(".Sidenavbar")
+var sidenavbar_close=document.getElementById("sidenavbar_close")
+
+
 var slidermargin=0
 
 rightarrow.addEventListener("click",function(){
@@ -43,4 +48,14 @@ leftarrow.addEventListener("click",function(){
 
 offer_section_close.addEventListener("click",function(){
   offer_section.style.display="none"
+})
+
+
+Navbar_container_sub3_menu.addEventListener("click",function(){
+  Sidenavbar.style.left="0%"
+
+})
+
+sidenavbar_close.addEventListener("click",function(){
+  Sidenavbar.style.left="-50%"
 })
